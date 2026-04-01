6fc51499a1d97f9b182d882c9b8792686a5d2d57
kosendorf <kosendorf@gmail.com>
2026-04-01 12:52:28 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'v1.0.12';
+const VERSION = 'v1.0.13';
 const CACHE_NAME = `latt-calc-${VERSION}`;
 
 const APP_STATIC_RESOURCES = [
@@ -38,7 +38,7 @@ self.addEventListener('fetch', event => {
 				// Cache any new successful responses
 				if (response && response.status === 200 && response.type === 'basic') {
 					//const clone = response.clone();
-					caches.open(CACHE_NAME).then(cache => cache.put(event.request, response));
+					caches.open(CACHE_NAME).then(cache => cache.add(response));
 				}
 				return response;
 			});
